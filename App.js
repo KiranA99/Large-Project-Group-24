cedb4203c1a2d638f6f16ed385e3915e6662d9bc
KiranA99 <84406470+KiranA99@users.noreply.github.com>
2021-07-02 20:11:01 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App"  color="white">
+      <h1>Health N' Wellness</h1> 
+      <div class="loginbox">
+      <h1 > Login </h1>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
